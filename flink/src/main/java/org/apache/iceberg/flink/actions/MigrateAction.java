99cd15fb62ae2848e207b909c84bd8bf3587464d
zhangjun <jun.zhang1@ly.com>
2020-11-05 17:38:03 +0800

    MigrateAction

--- flink/src/main/java/org/apache/iceberg/flink/actions/MigrateAction.java
+++ flink/src/main/java/org/apache/iceberg/flink/actions/MigrateAction.java
@@ -19,14 +19,73 @@
 
 package org.apache.iceberg.flink.actions;
 
+import java.util.List;
+import org.apache.flink.table.catalog.ObjectPath;
+import org.apache.flink.table.catalog.hive.HiveCatalog;
+import org.apache.iceberg.PartitionSpec;
+import org.apache.iceberg.Schema;
+import org.apache.iceberg.Table;
 import org.apache.iceberg.actions.Action;
+import org.apache.iceberg.catalog.Catalog;
+import org.apache.iceberg.catalog.Namespace;
+import org.apache.iceberg.catalog.TableIdentifier;
+import org.apache.iceberg.flink.FlinkCatalog;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class MigrateAction implements Action {
+  private static final Logger LOG = LoggerFactory.getLogger(MigrateAction.class);
 
-  private org.apache.flink.table.catalog.hive.HiveCatalog hiveCatalog;
+  private HiveCatalog hiveCatalog;
+  private Catalog icebergCatalog;
+  private String[] baseNamespace;
+  private FlinkCatalog flinkCatalog;
+
 
   @Override
   public Object execute() {
+
+    ObjectPath tablePath = null;
+
+    try {
+      flinkCatalog.createTable(tablePath, null, false);
+      TableIdentifier identifier = null;
+      Table table = icebergCatalog.loadTable(identifier);
+
+      importTable(table, hiveCatalog);
+    } catch (Exception e) {
+      LOG.error("eee", e);
+    }
+
+
     return null;
+  }
+
+  private void importTable(Table table, HiveCatalog catalog) throws Exception {
+
+    // load hive table
+
+    ObjectPath tablePath = new ObjectPath("", "");
+    org.apache.hadoop.hive.metastore.api.Table hiveTable = catalog.getHiveTable(tablePath);
+    String location = hiveTable.getSd().getLocation();
+
+
+  }
+
+  TableIdentifier toIdentifier(ObjectPath path) {
+    return TableIdentifier.of(toNamespace(path.getDatabaseName()), path.getObjectName());
+  }
+
+  private Namespace toNamespace(String database) {
+    String[] namespace = new String[baseNamespace.length + 1];
+    System.arraycopy(baseNamespace, 0, namespace, 0, baseNamespace.length);
+    namespace[baseNamespace.length] = database;
+    return Namespace.of(namespace);
+  }
+
+  private static PartitionSpec toPartitionSpec(List<String> partitionKeys, Schema icebergSchema) {
+    PartitionSpec.Builder builder = PartitionSpec.builderFor(icebergSchema);
+    partitionKeys.forEach(builder::identity);
+    return builder.build();
   }
 }
